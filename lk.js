bfe9776853653cb723978db87ac98c5ad5845fae
reality <tinmachin3@gmail.com>
2015-09-22 21:26:34 +0000

    nesting is hard :(

--- lk.js
+++ lk.js
@@ -2,6 +2,7 @@ var _ = require('underscore')._;
 
 var isAxiom = function(input) {
   if(input[0].length == 1 && input[1].length == 1
+      && _.isString(input[0][0])
       && input[0][0].match(/^[A-Z]$/)
       && input[0][0] == input[1][0]) {
     return true;
@@ -10,41 +11,51 @@ var isAxiom = function(input) {
   }
 };
 
+var prettyOperation = function(item) {
+  var out = item;
+  if(_.isObject(item) && _.has(item, 'operation')) {
+    if(item.operation == 'implies') {
+      if(_.isObject(item.p1)) {
+        item.p1 = '('+prettyOperation(item.p1)+')';
+      }
+      if(_.isObject(item.p2)) {
+        item.p2 = '('+prettyOperation(item.p2)+')';
+      }
+      out = item.p1 + ' → ' + item.p2;
+    }
+  }
+  return out;
+};
+
 var inferenceRules = [
   // LEFT RULES 
 
   function(input) { // AL1
     var left = input[0],
         right = input[1],
-        pattern = null,
+        success = false,
         element = _.last(left);
         
-    if(element) {
-      pattern = element.match(/^and\((.+?),\s?(.+)\)$/i);
+    if(element && element.operation == 'and') {
+      left.splice(-1, 1, element.p1);
+      success = true;
     }
 
-    if(pattern) {
-      left.push(pattern[1]);
-    }
-
-    return [ pattern != null, 'AL1', input ];
+    return [ success, 'AL1', input ];
   },
 
   function(input) { // AL2
     var left = input[0],
         right = input[1],
-        pattern = null,
+        success = false,
         element = _.last(left);
         
-    if(element) {
-      pattern = element.match(/^and\((.+?),\s?(.+)\)$/i);
+    if(element && element.operation == 'and') {
+      left.splice(-1, 1, element.p2);
+      success = true;
     }
 
-    if(pattern) {
-      left[0] = pattern[2];
-    }
-
-    return [ pattern != null, 'AL2', input ];
+    return [ success, 'AL2', input ];
   },
 
   /** TODO: SPLIT RULES
@@ -69,19 +80,15 @@ var inferenceRules = [
   function(input) { // NL
     var left = input[0],
         right = input[1],
-        pattern = null,
+        success = false,
         element = _.last(left);
         
-    if(element) {
-      pattern = element.match(/^not\((.+)\)$/i);
-    }
-
-    if(pattern) {
-      right.push(pattern[1]); // this may actually have to be prepended
+    if(element && element.operation == 'not') {
+      right.push(element.p1); // this may actually have to be prepended
       left.splice(-1);
     }
 
-    return [ pattern != null, 'NL', input ];
+    return [ success, 'NL', input ];
   },
 
   // Structural Rules
@@ -134,75 +141,54 @@ var inferenceRules = [
   function(input) { // AR1
     var left = input[0],
         right = input[1],
-        pattern = null;
+        success = false;
         
-    if(right[0]) {
-      pattern = right[0].match(/^or\((.+?),\s?(.+)\)$/i);
+    if(right[0] && right[0].operation == 'or') {
+      right[0] = right[0].p1;
+      success = true;
     }
 
-    if(pattern) {
-      right[0] = pattern[1];
-    }
-
-    return [ pattern != null, 'AR1', input ];
+    return [ success, 'AR1', input ];
   },
 
   function(input) { // AR2
     var left = input[0],
         right = input[1],
-        pattern = null;
+        success = false;
         
-    if(right[0]) {
-      pattern = right[0].match(/^or\((.+?),\s?(.+)\)$/i);
+    if(right[0] && right[0].operation == 'or') {
+      right[0] = right[0].p2;
+      success = true;
     }
 
-    if(pattern) {
-      right[0] = pattern[2];
-    }
-
-    return [ pattern != null, 'AR2', input ];
+    return [ success, 'AR2', input ];
   },
 
   function(input) { // IR
     var left = input[0],
         right = input[1],
-        pattern = null;
+        success = false;
         
-    if(right[0]) {
-      pattern = right[0].match(/^implies\((.+?),\s?(.+)\)$/i);
+    if(right[0] && right[0].operation == 'implies') {
+      left.push(right[0].p1)
+      right[0] = right[0].p2;
+      success = true;
     }
 
-    if(pattern) {
-    console.log('pat');
-    console.log(pattern);
-console.log('before ir');
-console.log(right);
-      right[0] = pattern[2];
-      left.push(pattern[1]);
-console.log('after ir');
-console.log(right);
-    }
-
-    return [ pattern != null, 'IR', input ];
+    return [ success, 'IR', input ];
   },
 
   function(input) { // NR
     var left = input[0],
         right = input[1],
-        pattern = null;
+        success = false;
         
-    if(right[0]) {
-      pattern = right[0].match(/^not\((.+)\)$/i);
-    }
-
-    if(pattern) {
-      left.push(pattern[1]);
+    if(right[0] && right[0].operation == 'not') {
+      left.push(right[0].p1);
       right.splice(0, 1);
-      console.log('nr success');
-      console.log(input);
     }
 
-    return [ pattern != null, 'NR', input ];
+    return [ success, 'NR', input ];
   },
 
   // Structural Rules
@@ -211,7 +197,11 @@ console.log(right);
     var left = input[0],
         right = input[1];
 
+//console.log('before wr');
+//console.log(right);
     right.splice(0, 1);
+//console.log('after wr');
+//console.log(right);
 
     return [ true, 'WR', input ];
   },
@@ -323,7 +313,15 @@ var reason = function(input) {
       _.each(solutionFound, function(val, step) {
         console.log('    Step: ' + (step+1));
         console.log('      Operation: ' + val[0]);
-        console.log('      Value: ' + val[1][0] + ' ⇒ ' + val[1][1]);
+
+        _.each(val[1][0], function(op, i) {
+          val[1][0][i] = prettyOperation(op); 
+        });
+        _.each(val[1][1], function(op, i) {
+          val[1][1][i] = prettyOperation(op); 
+        });
+
+        console.log('      Value: ' + val[1][0] + ' ⊢ ' + val[1][1]);
       });
 
       console.log();
@@ -337,19 +335,64 @@ var reason = function(input) {
 
     tracks = nextTracks;
 
-/*    console.log('Round ' + x + ' complete! Tracks: ');
+    console.log('Round ' + x + ' complete! Tracks: ');
     _.each(tracks, function(track, i) {
       console.log('  Track ' + i);
       _.each(track, function(val, step) {
         console.log('    Step ' + step);
         console.log('      ' + val);
       });
-    });*/
+    });
+
+//    if(x==2) break;
   }
 };
 
 // Each array symbolises its respective side of the sequent ⇒
 //var input = [[], ['implies(implies(A,implies(B,C)),implies(implies(A,B),implies(A,C)))']];
-var input = [[], ['implies(A,implies(B,A))']]
+
+/*var input = [ [], [
+  {
+    'operation': 'implies',
+    'p1': { 
+      'operation': 'implies',
+      'p1': {
+        'operation': 'implies',
+        'p1': 'B',
+        'p2': 'C'
+      },
+      'p2': {
+        'operation': 'implies',
+        'p1': 'B',
+        'p2': 'C'
+      }
+    },
+    'p2': {
+      'operation': 'implies',
+      'p1': {
+        'operation': 'implies',
+        'p1': 'A',
+        'p2': 'B'
+      },
+      'p2': {
+        'operation': 'implies',
+        'p1': 'A',
+        'p2': 'C'
+      }
+    }
+  }
+]];*/
+
+var input = [[], [
+  {
+    'operation': 'implies',
+    'p1': 'A',
+    'p2': {
+      'operation': 'implies',
+      'p1': 'B',
+      'p2': 'A'
+    }
+  }
+]];
 
 reason(input);
