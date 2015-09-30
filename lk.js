4a4dfccbc474404e776448c55f5321505fd2f4db
Luke Slater <tinmachin3@gmail.com>
2015-09-30 22:32:27 +0300

    more proofs

--- lk.js
+++ lk.js
@@ -583,7 +583,7 @@ var reason = function(input) {
     formula = nextTracks;
 
     console.log('Round ' + x + ' complete! Tracks: ' + formula.length);
-    _.each(formula, function(track, i) {
+    /*_.each(formula, function(track, i) {
         console.log('  Track ' + i);
         _.each(track, function(step, s) {
           console.log('    Step ' + s);
@@ -604,7 +604,7 @@ var reason = function(input) {
             console.log('      Value: ' + prettified[0].join(', ') + ' ⊢ ' + prettified[1].join(', '));
           });
         });
-      });
+      });*/
 
     if(solutionFound) {
       console.log();
@@ -643,6 +643,14 @@ var reason = function(input) {
     //if(x==1) break;
   }
 };
+/*
+var input = [[], [
+  {
+    'operation': 'implies',
+    'p1': 'A',
+    'p2': 'A'
+  }
+]];
 
 // Hilbert #2
 /*var input = [[], [
@@ -655,10 +663,11 @@ var reason = function(input) {
       'p2': 'A'
     }
   }
-]];*/
+]];
+
 
 // hilbert 4m
-var input = [ [], [
+/*var input = [ [], [
   {
     'operation': 'implies',
     'p1': { 
@@ -682,22 +691,21 @@ var input = [ [], [
       }
     }
   }
-]];
+]];*/
 
 // hilbert #3
-/*var input = [ [], [
-  {
+var input = [ [
+  { 
     'operation': 'implies',
-    'p1': { 
-      'operation': 'implies',
-      'p1': 'A',
-      'p2': {
-        'operation': 'implies',
-        'p1': 'B',
-        'p2': 'C'
-      }
-    },
+    'p1': 'A',
     'p2': {
+      'operation': 'implies',
+      'p1': 'B',
+      'p2': 'C'
+    }
+  }
+], [
+  {
       'operation': 'implies',
       'p1': {
         'operation': 'implies',
@@ -709,10 +717,9 @@ var input = [ [], [
         'p1': 'A',
         'p2': 'C'
       }
-    }
   }
 ]];
-*/
+
 /*var input = [ [], [ {
     'operation': 'implies',
     'p1': {
